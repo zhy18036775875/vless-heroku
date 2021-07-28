6348699060bfb092dd43927cc313ac1ce0be041c
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-28 19:53:05 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,10 +10,56 @@ xray -version
 # Remove temporary directory
 rm -rf /tmp/xray
 
+# VLESS new configuration
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "listen": "/etc/caddy/vless",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$ID",
+                        "flow": "xtls-rprx-direct",
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "allowInsecure": false,
+                "wsSettings": {
+                   "path": "/$ID-vless?ed=2048"
+                }
+            },
+            "sniffing": {
+                "enabled": true,
+                "destOverride": [
+                    "http",
+                    "tls"
+                ]
+            }
+        }
+    ],
+    "outbounds": [ 
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
 # Config Caddy
 mkdir -p /etc/caddy/ /usr/share/caddy/ && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
-wget ${CADDYIndexPage} -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget -qO- ${CONFIGCADDY} | sed -e "1c :${PORT}" -e "s/\${ID}/${ID}/g" -e "s/\${MYUUID-HASH}/${(caddy hash-password --plaintext ${ID})}/g" >/etc/caddy/Caddyfile
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
 
 # Run VLESS
 tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
