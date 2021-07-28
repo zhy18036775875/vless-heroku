070fedff05329afbc62cd2b5caff8bbd8ac2b87e
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-28 18:26:44 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -13,7 +13,7 @@ rm -rf /tmp/xray
 # Config Caddy
 mkdir -p /etc/caddy/ /usr/share/caddy/ && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 curl -fssL $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-curl -fssL $CONFIGCADDY -O /etc/caddy/Caddyfile | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
+curl -fssL $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
 
 # Run VLESS
 tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
