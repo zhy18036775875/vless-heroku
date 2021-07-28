9b538662cafa7d8708d097eef53e48c659ce31b0
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-28 19:11:33 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,10 +10,11 @@ xray -version
 # Remove temporary directory
 rm -rf /tmp/xray
 
-# Config Caddy
+# Config VLESS
 mkdir -p /etc/caddy/ /usr/share/caddy/ && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
+wget -qO- https://raw.githubusercontent.com/Dimitri2020007/vless-heroku/main/config.json | sed -e "s/\$ID/$ID/g">/usr/local/etc/xray/config.json
 
 # Run VLESS
 tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
