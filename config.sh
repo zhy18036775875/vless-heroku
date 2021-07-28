54abe9cd04d18b749acdfe18a8758a44bedd8566
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-28 16:30:17 +0800

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Download and install VLESS
+mkdir -p /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+xray -version
+
+# Remove temporary directory
+rm -rf /tmp/xray
+
+# Run VLESS
+tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
