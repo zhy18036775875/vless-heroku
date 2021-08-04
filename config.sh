05576b06ed56ce4b4d8726d8ba042342a1fc3d07
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-04 19:24:25 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -57,13 +57,8 @@ EOF
 
 # Other configure
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-sed -e "/^#/d"\
-    -e "1c :$PORT"\
-    -e "s/\$ID/$ID/g"\
-    -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g"\
-    -e "$s"\
-    /conf/Caddyfile > /etc/caddy/Caddyfile
-    echo /etc/caddy/Caddyfile
+sed -e "/^#/d" -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" -e "$s" /conf/Caddyfile > /etc/caddy/Caddyfile
+echo /etc/caddy/Caddyfile
 
 # Run VLESS
 tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
