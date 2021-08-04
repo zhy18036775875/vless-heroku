d1ab50aa32f814841ab9f7ff78e808db829b5e5a
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-04 16:53:39 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -42,7 +42,7 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "network": "ws",
                 "allowInsecure": false,
                 "wsSettings": {
-                   "path": "/$ID-vless?ed=2048"
+                   "path": "/$ID-vless"
                 }
             },
             "sniffing": {
