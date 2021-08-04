2f7049ffbe2a1f24957b2b787e726ae02512b49a
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-04 18:53:28 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -48,41 +48,11 @@ EOF
 
 # Config Caddy
 mkdir -p /etc/caddy/ /usr/share/caddy/
-
-# Caddy config
-cat >> Caddyfile <<EOF > /etc/caddy/Caddyfile
-:$PORT
-
-root * /usr/share/caddy
-file_server browse
-
-header {
-    X-Robots-Tag none
-    X-Content-Type-Options nosniff
-    X-Frame-Options DENY
-    Referrer-Policy no-referrer-when-downgrade
-}
-
-basicauth /$ID/* {
-    $ID $MYUUID-HASH
-}
-
-@websocket_xray_vless {
-    header Connection *Upgrade*
-    header Upgrade    websocket
-    path /$ID-vless
-}
-reverse_proxy @websocket_xray_vless unix//etc/caddy/vless
-EOF
-
-# Robot.txt config
 install -d /usr/share/caddy/
 cat << EOF > /usr/share/caddy/robots.txt
 User-agent: *
 Disallow: /
 EOF
-
-# Other configuration
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
 
