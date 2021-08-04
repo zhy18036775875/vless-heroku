92eabefc307fb5528e3ef8d6c1c5d0fa29db0554
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-04 18:06:03 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # VLESS new configuration
-install -d /usr/local/etc/xray
+install -d /usr/local/etc/xray/
 cat << EOF > /usr/local/etc/xray/config.json
 {
     "log": {
@@ -48,19 +48,44 @@ EOF
 
 # Config Caddy
 mkdir -p /etc/caddy/ /usr/share/caddy/
+
+# Caddy config
+install -d /etc/caddy/
+cat << EOF > /etc/caddy/Caddyfile
+:$PORT
+
+root * /usr/share/caddy
+file_server browse
+
+header {
+    X-Robots-Tag none
+    X-Content-Type-Options nosniff
+    X-Frame-Options DENY
+    Referrer-Policy no-referrer-when-downgrade
+}
+
+basicauth /$ID/* {
+    $ID $MYUUID-HASH
+}
+
+@websocket_xray_vless {
+    header Connection *Upgrade*
+    header Upgrade    websocket
+    path /$ID-vless
+}
+reverse_proxy @websocket_xray_vless unix//etc/caddy/vless
+EOF
+sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
+
+# Robot.txt config
 install -d /usr/share/caddy/
 cat << EOF > /usr/share/caddy/robots.txt
 User-agent: *
 Disallow: /
 EOF
-#wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-sed -e "/^#/d"\
-    -e "1c :$PORT"\
-    -e "s/\$ID/$ID/g"\
-    -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g"\
-    -e "$s"\
-    /conf/Caddyfile > /etc/caddy/Caddyfile
-    echo /etc/caddy/Caddyfile
+
+# Other configuration
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 
 # Run VLESS
 tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
