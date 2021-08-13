67a2fceff34a75a3ac8b6a77ba2e41b4e5820f9c
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-13 12:32:55 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -58,6 +58,7 @@ EOF
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 sed -e "/^#/d" -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --algorithm bcrypt --plaintext $ID)/g" -e "$s" /conf/Caddyfile > /etc/caddy/Caddyfile
 echo /etc/caddy/Caddyfile
+cp -r /conf/envfile /usr/local/bin/envfile
 
 # Run VLESS
-tor & /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json & /usr/local/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile 
+tor & /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json & /usr/local/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile ---envfile /usr/local/bin/envfile
