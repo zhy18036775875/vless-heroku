184f357e4c1ee097cf33b8ab59fcde43d9a21638
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-13 14:15:51 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # VLESS new configuration
-
+mkdir -p  /usr/local/bin/v2ray
+cd /usr/local/bin/v2ray
+wget -qO- "https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip" | unzip v2ray-linux-64.zip
+rm -rf v2ray-linux-64.zip
+chmod +x ./v2ray
+chmod +x ./v2ctl
+ls -al
 
 # Decompress webroot
 cd /usr/share/nginx/wwwroot/
@@ -27,4 +33,4 @@ echo /etc/nginx/conf.d/ray.conf
 cat /etc/nginx/conf.d/ray.conf
 
 # Run VLESS
-tor & /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json & rm -rf /etc/nginx/sites-enabled/default & nginx -g 'daemon off;'
+tor & ./usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json & rm -rf ./etc/nginx/sites-enabled/default & nginx -g 'daemon off;'
