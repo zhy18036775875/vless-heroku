2be886e14d0dc4614231dcb40c928aa0a406ed02
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-20 23:43:22 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -39,4 +39,4 @@ cat > /usr/local/etc/v2ray/config.json << EOF
 EOF
 
 # Run vless
-tor & /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
