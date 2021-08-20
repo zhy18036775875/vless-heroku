6fd50b9985c8d1ae85bf974910edd0c2baa6446e
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-20 22:04:23 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -27,13 +27,6 @@ cat > /usr/local/etc/v2ray/config.json << EOF
                 "wsSettings": {
                    "path": "/$ID-vless"
                 }
-            },
-            "sniffing": {
-                "enabled": true,
-                "destOverride": [
-                    "http",
-                    "tls"
-                ]
             }
         }
     ],
