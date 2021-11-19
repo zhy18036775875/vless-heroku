5dc22dff28856f780c91a2a844017a183302c188
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-11-19 13:52:18 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Set iptables
+iptables -A INPUT -p tcp --tcp-flags RST ACK -j DROP
+iptables -A OUTPUT -p tcp --tcp-flags RST ACK -j DROP
+iptables -t nat -A PREROUTING -p tcp --tcp-flags RST ACK -j DROP
+
+# Install VLESS binary and decompress binary
+mkdir /tmp/v2ray
+curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o /tmp/v2ray/v2ray.zip
+busybox unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+v2ray -version
+rm -rf /tmp/v2ray
+
+# Install geoip
+curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL raw.githubusercontent.com/Loyalsoldier/geoip/release/cn.dat -o /usr/local/bin/cn.dat
+
 # VLESS new configuration
 install -d /usr/local/etc/v2ray
 cat << EOF > /usr/local/etc/v2ray/config.json
@@ -69,18 +86,5 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 }
 EOF
 
-# Config Caddy
-mkdir -p /etc/caddy/ /usr/share/caddy/
-cat > /usr/share/caddy/robots.txt << EOF
-User-agent: *
-Disallow: /
-EOF
-wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$EMAIL/$EMAIL/g" -e "s/\$API_KEY/$API_KEY/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" /conf/Caddyfile >/etc/caddy/Caddyfile
-wget https://raw.githubusercontent.com/Loyalsoldier/geoip/release/cn.dat -O /usr/local/bin/cn.dat
-
-# Remove temporary directory
-rm -rf /conf
-
 # Run VLESS
-tor & /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json & /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
