b877424b1ad6cdfa2e22926f4efcc274087f6a35
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-11-22 17:41:11 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Install VLESS binary and decompress binary
+# Install V2/X2 binary and decompress binary
 mkdir /tmp/v2ray
 curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o /tmp/v2ray/v2ray.zip
 busybox unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
@@ -12,7 +12,7 @@ rm -rf /tmp/v2ray
 # Install geoip
 curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL raw.githubusercontent.com/Loyalsoldier/geoip/release/cn.dat -o /usr/local/bin/cn.dat
 
-# VLESS new configuration
+# V2/X2 new configuration
 install -d /usr/local/etc/v2ray
 cat << EOF > /usr/local/etc/v2ray/config.json
 {
@@ -81,5 +81,5 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 }
 EOF
 
-# Run VLESS
+# Run V2/X2
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
